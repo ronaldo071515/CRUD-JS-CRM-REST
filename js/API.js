20d2dd268a10360d38225e10cbbb152c1d1288ea
Ronaldo Torres <rtorres66@misena.edu.co>
2022-05-19 18:05:34 -0500

    finalizado proyecto crud usando async y await CMR-CRUD JSON-SERVER

---
+++ js/API.js
@@ -0,0 +1,69 @@
+const url = `http://localhost:3000/clientes`;
+
+//Crear Cliente
+export const nuevoCliente = async cliente => {
+
+    try {
+        await fetch( url, {
+            method: 'POST',
+            body: JSON.stringify( cliente ),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html'
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+//Obtener toodos los clientes
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch( url );
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Elimina un cliente
+export const eliminarCliente = async id => {
+    try {
+        await fetch( `${ url }/${ id }`, {
+            method: 'DELETE',
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Obtiene cliente por ID
+export const obtenerCliente = async id => {
+    try {
+        const resultado = await fetch(`${ url }/${ id }`);
+        const cliente = await resultado.json();
+        return cliente;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Actualiza un registro
+export const editarCliente = async cliente => {
+    try {
+        await fetch(`${ url }/${ cliente.id }`, {
+            method: 'PUT',
+            body: JSON.stringify( cliente ),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+
+}
